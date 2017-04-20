6f0449b54ed0708fdc97361a3fbc7ee27b4679b1
MartaPiatek <martusia.piatek@gmail.com>
2017-04-20 22:49:21 +0200

    Update implementation of Details Activity, closes #17

--- PremiereTrackerApp/app/src/main/java/com/example/karolinar/premieretracker/DetailsActivity.java
+++ PremiereTrackerApp/app/src/main/java/com/example/karolinar/premieretracker/DetailsActivity.java
@@ -4,17 +4,58 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    TextView txtViewNameValue, txtViewCategoryValue, txtViewPremiereValue, txtViewCreatorValue, txtViewDescriptionValue;
+    DatabaseManager dbManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        TextView txtViewNameValue = (TextView) findViewById(R.id.txtViewNameValue);
+        TextView txtViewCategoryValue = (TextView) findViewById(R.id.txtViewCategoryValue);
+        TextView txtViewCreatorValue = (TextView) findViewById(R.id.txtViewCreatorValue);
+        TextView txtViewDescriptionValue = (TextView) findViewById(R.id.txtViewDescriptionValue);
+        TextView txtViewPremiereValue = (TextView) findViewById(R.id.txtViewPremiereDateValue);
+
+        dbManager=new DatabaseManager(this);
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+        ProductEntity p1= new ProductEntity();
+        p1.ProductType="Film";
+        p1.Name="TestFilm";
+        p1.Creator="autor";
+        p1.Description="Opis";
+        p1.Premiere= new Date();
+
+        dbManager.AddProduct(p1);
+
+        List<ProductEntity> products = new LinkedList<ProductEntity>();
+        products=dbManager.GetProducts();
+
+        ProductEntity pe = new ProductEntity();
+
+        if(products.size()>0){
+
+            // TODO przekazywanie id produktu z ObservedListActivity
+            pe = products.get(1);
+            txtViewNameValue.setText(pe.Name);
+            txtViewCategoryValue.setText(pe.ProductType);
+            txtViewCreatorValue.setText(pe.Creator);
+            txtViewPremiereValue.setText(format.format(pe.Premiere));
+            txtViewDescriptionValue.setText(pe.Description);
+        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +65,7 @@ public class DetailsActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
     }
 
 }
