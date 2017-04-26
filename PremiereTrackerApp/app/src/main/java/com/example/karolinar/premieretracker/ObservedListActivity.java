16d186a31ebec0e315f644598f555cb8957d5f89
Dominika Buksak <193982@student.pwr.edu.pl>
2017-04-26 23:22:41 +0200

    Fixed observed list implementation, closes #24

--- PremiereTrackerApp/app/src/main/java/com/example/karolinar/premieretracker/ObservedListActivity.java
+++ PremiereTrackerApp/app/src/main/java/com/example/karolinar/premieretracker/ObservedListActivity.java
@@ -1,5 +1,6 @@
 package com.example.karolinar.premieretracker;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -29,45 +30,61 @@ import java.util.List;
 
 public class ObservedListActivity extends AppCompatActivity {
 
-    private DatabaseManager databaseManager = new DatabaseManager(this);
-    private SQLiteDatabase sqLiteDatabase = databaseManager.getWritableDatabase();
-    private Cursor todoCursor = sqLiteDatabase.rawQuery("SELECT * FROM Products", null);
-    private ObservedProductsAdapter observedProductsAdapter = new ObservedProductsAdapter(this, todoCursor);
-    private ListView listObservedProducts = (ListView) findViewById(R.id.listObservedProducts);
-    private ArrayList<Product> list = new ArrayList<>();
+
+    private ObservedProductsAdapter observedProductsAdapter;
+    private ListView listObservedProducts;
+    DatabaseManager databaseManager = new DatabaseManager(this);
+    ArrayList<ProductEntity>  listProducts = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_observed_list);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_observed_list);
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+            listObservedProducts = (ListView) findViewById(R.id.listObservedProducts);
+            List<ProductEntity> productEntityList = new ArrayList<>();
+            productEntityList = databaseManager.GetProducts();
+            listProducts.addAll(productEntityList);
+            observedProductsAdapter = new ObservedProductsAdapter(this, R.layout.observed_list_view, listProducts);
+            listObservedProducts.setAdapter(observedProductsAdapter);
+            Spinner spinnerCategory = (Spinner) findViewById(R.id.spinnerCategory);
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                    R.array.content_types_array, android.R.layout.simple_spinner_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinnerCategory.setAdapter(adapter);
 
-        listObservedProducts = (ListView) findViewById(R.id.listObservedProducts);
-        listObservedProducts.setAdapter(observedProductsAdapter);
-        Spinner spinnerCategory = (Spinner) findViewById(R.id.spinnerCategory);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.content_types_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerCategory.setAdapter(adapter);
-        ImageButton btnDelete = (ImageButton) findViewById(R.id.btnDelete);
-        btnDelete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                int position = (int) arg0.getTag();
-                databaseManager.RemoveProduct(position);
-                observedProductsAdapter.notifyDataSetChanged();
-            }
-        });
+            Button btnSearch = (Button) findViewById(R.id.btnSearch);
+            btnSearch.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    fillTable();
+                }
+            });
+    }
 
+    private void fillTable(){
+        listProducts.clear();
+        databaseManager.GetProducts();
+        try{
+            List<ProductEntity> productEntityList = new ArrayList<>();
+            productEntityList = databaseManager.GetProducts();
+            listProducts.addAll(productEntityList);
+        }catch (Exception e){
+            System.out.println("Brak Produktow w bazie");
+        }
+        observedProductsAdapter.notifyDataSetChanged();
+    }
+
+    public boolean exists(Product p){
+        ProductEntity entity = new ProductEntity();
+        entity.Description = "";
+        entity.Name = p.getTitle();
+        entity.Premiere = p.getPremiereDate();
+        entity.ProductType = p.getClass().getSimpleName();
+        entity.Creator = "";
+        DatabaseManager db = new DatabaseManager(this);
+        return db.existsProduct(entity);
     }
 }
