9d54a65497659538ce0652ccbd5a43364e5a8fd3
Dominika Buksak <193982@student.pwr.edu.pl>
2017-05-06 08:33:16 +0200

    Add display info about clicked product

--- PremiereTrackerApp/app/src/main/java/com/example/karolinar/premieretracker/DetailsActivity.java
+++ PremiereTrackerApp/app/src/main/java/com/example/karolinar/premieretracker/DetailsActivity.java
@@ -13,6 +13,8 @@ import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
+import static android.R.attr.format;
+
 public class DetailsActivity extends AppCompatActivity {
 
     TextView txtViewNameValue, txtViewCategoryValue, txtViewPremiereValue, txtViewCreatorValue, txtViewDescriptionValue;
@@ -23,39 +25,23 @@ public class DetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-        //TextView txtViewNameValue = (TextView) findViewById(R.id.txtViewNameValue);
-        //TextView txtViewCategoryValue = (TextView) findViewById(R.id.txtViewCategoryValue);
-        //TextView txtViewCreatorValue = (TextView) findViewById(R.id.txtViewCreatorValue);
-        //TextView txtViewDescriptionValue = (TextView) findViewById(R.id.txtViewDescriptionValue);
-        //TextView txtViewPremiereValue = (TextView) findViewById(R.id.txtViewPremiereDateValue);
+        TextView txtViewNameValue = (TextView) findViewById(R.id.txtViewNameValue);
+        TextView txtViewCategoryValue = (TextView) findViewById(R.id.txtViewCategoryValue);
+        TextView txtViewCreatorValue = (TextView) findViewById(R.id.txtViewCreatorValue);
+        TextView txtViewDescriptionValue = (TextView) findViewById(R.id.txtViewDescriptionValue);
+        TextView txtViewPremiereValue = (TextView) findViewById(R.id.txtViewPremiereDateValue);
 
+        String productId = getIntent().getStringExtra("PRODUCT_ID");
         dbManager=new DatabaseManager(this);
+        ProductEntity productEntity = dbManager.getProductById(productId);
         DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 
-        ProductEntity p1= new ProductEntity();
-        p1.ProductType="Film";
-        p1.Name="TestFilm";
-        p1.Creator="autor";
-        p1.Description="Opis";
-        p1.Premiere= new Date();
+        txtViewNameValue.setText(productEntity.Name);
+        txtViewCategoryValue.setText(productEntity.ProductType);
+        txtViewCreatorValue.setText(productEntity.Creator);
+        txtViewPremiereValue.setText(format.format(productEntity.Premiere));
+        txtViewDescriptionValue.setText(productEntity.Description);
 
-        dbManager.AddProduct(p1);
-
-        List<ProductEntity> products = new LinkedList<ProductEntity>();
-        products=dbManager.GetProducts();
-
-        ProductEntity pe = new ProductEntity();
-
-        if(products.size()>0){
-
-            // TODO przekazywanie id produktu z ObservedListActivity
-            pe = products.get(1);
-            txtViewNameValue.setText(pe.Name);
-            txtViewCategoryValue.setText(pe.ProductType);
-            txtViewCreatorValue.setText(pe.Creator);
-            txtViewPremiereValue.setText(format.format(pe.Premiere));
-            txtViewDescriptionValue.setText(pe.Description);
-        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
