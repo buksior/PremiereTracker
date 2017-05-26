a383699a4065fd11ebbcff6247700b545c24c19c
MartaPiatek <martusia.piatek@gmail.com>
2017-05-26 11:52:11 +0200

    Add Email Contact Form

--- PremiereTrackerApp/app/src/main/java/com/example/karolinar/premieretracker/ContactActivity.java
+++ PremiereTrackerApp/app/src/main/java/com/example/karolinar/premieretracker/ContactActivity.java
@@ -1,7 +1,10 @@
 package com.example.karolinar.premieretracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class ContactActivity extends AppCompatActivity {
 
@@ -9,6 +12,18 @@ public class ContactActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+        Button btnMessage = (Button) findViewById(R.id.btnMessage);
+        btnMessage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent startIntent = new Intent(getApplicationContext(),MailContactActivity.class);
+                startActivity(startIntent);
+            }
+        });
     }
+
 
 }
---
+++ PremiereTrackerApp/app/src/main/java/com/example/karolinar/premieretracker/MailContactActivity.java
@@ -0,0 +1,38 @@
+package com.example.karolinar.premieretracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MailContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mail_contact);
+
+        Button btnOk = (Button) findViewById(R.id.btnOK);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                String to = ((EditText) findViewById(R.id.txtTo)).getText().toString();
+                String sub = ((EditText) findViewById(R.id.txtSubject)).getText().toString();
+                String mess = ((EditText) findViewById(R.id.txtMessage)).getText().toString();
+                Intent mail = new Intent(Intent.ACTION_SEND);
+                mail.putExtra(Intent.EXTRA_EMAIL, new String[]{to});
+                mail.putExtra(Intent.EXTRA_SUBJECT, sub);
+                mail.putExtra(Intent.EXTRA_TEXT, mess);
+                mail.setType("message/rfc822");
+                startActivity(Intent.createChooser(mail, "Send email via:"));
+                Toast.makeText(MailContactActivity.this.getApplicationContext(), "Wysłano wiadomość", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
